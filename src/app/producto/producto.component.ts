44bb1951859072498d36d940eb8f32d89393b41b
paula75 <pportega@uc.cl>
2022-01-14 20:40:30 -0300

    Agrega funcionalidades imagen

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
-
+  leche : Producto[] = [
+    {
+      name: 'Soprole',
+      valor: 5
+    },
+    {
+      name: 'Loncoleche',
+      valor: 2
+    },
+    {
+      name: 'Soprole 2',
+      valor: 5
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
+
+export interface Producto {
+  name: string;
+  valor: number;
+
+};
