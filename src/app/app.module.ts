fd0edc98e46967ff5d3b360ac33d12c5364f6492
paula75 <pportega@uc.cl>
2022-01-14 19:57:35 -0300

    Agrega topbar y card de producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    ProductoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu-bar/menu-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.css']
+})
+export class MenuBarComponent implements OnInit {
+  tipoAnalisis = [
+    'Sal',
+    'Azúcar',
+    'Proteína',
+    'Grasa',
+    'H. de carbono',
+    'Colesterol'
+  ];
+
+
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+
+}
