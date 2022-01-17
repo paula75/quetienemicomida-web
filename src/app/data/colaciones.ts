25810d16a356efaab4a3ea93e19765ad58eb402e
paula75 <pportega@uc.cl>
2022-01-17 11:30:01 -0300

    Agrega info colaciones

---
+++ src/app/data/colaciones.ts
@@ -0,0 +1,209 @@
+import { Producto } from "../producto";
+
+
+export const TIPOSGALLETAS: Producto[] = [
+
+    {
+        nombre: 'Oreo Chocolate crema', 
+        marca: 'Oreo',
+        tipo: 'Chocolate crema',
+        energia: 480,
+        proteina: 4,
+        grasa: 20,
+        carbohidratos: 68,
+        colesterol: 0, 
+        azucar: 40,
+        sodio: 450,
+        imagen: 'assets/images/dulces/galletas/oreo_chocolate.png'
+    }, {
+        nombre: 'Costa Donuts leche', 
+        marca: 'Costa',
+        tipo: 'Donuts leche',
+        energia: 506,
+        proteina: 5.4,
+        grasa: 24,
+        carbohidratos: 67,
+        colesterol: 3.28, 
+        azucar: 35.6,
+        sodio: 267,
+        imagen: 'assets/images/dulces/galletas/costa_donuts.png'
+    }, {
+        nombre: 'McKay Kuky', 
+        marca: 'McKay',
+        tipo: 'Kuky',
+        energia: 506,
+        proteina: 6.8,
+        grasa: 22.1,
+        carbohidratos: 70,
+        colesterol: 2, 
+        azucar: 25.1,
+        sodio: 386,
+        imagen: 'assets/images/dulces/galletas/mckay_kuky.png'
+    },{
+        nombre: 'McKay Morocha', 
+        marca: 'McKay',
+        tipo: 'Morocha',
+        energia: 495,
+        proteina: 5.3,
+        grasa: 21,
+        carbohidratos: 71.2,
+        colesterol: 3, 
+        azucar: 37.2,
+        sodio: 170,
+        imagen: 'assets/images/dulces/galletas/mckay_morocha.png'
+    },{
+        nombre: 'Mizos Chocolate', 
+        marca: 'Mizos',
+        tipo: 'Chocolate',
+        energia: 396,
+        proteina: 5.9,
+        grasa: 12.9,
+        carbohidratos: 64,
+        azucar: 14.5,
+        sodio: 6,
+        imagen: 'assets/images/dulces/galletas/mizos_chocolate.png'
+    },{
+        nombre: 'Mizos Chocolate y naranja', 
+        marca: 'Mizos',
+        tipo: 'Chocolate y naranja',
+        energia: 440,
+        proteina: 5.6,
+        grasa: 18.1,
+        carbohidratos: 61.9,
+        azucar: 26,
+        sodio: 40,
+        imagen: 'assets/images/dulces/galletas/mizos_chocolatenaranja.png'
+    },{
+        nombre: 'Costa Chocochips', 
+        marca: 'Costa',
+        tipo: 'Chocochips',
+        energia: 517,
+        proteina: 4.3,
+        grasa: 25.9,
+        carbohidratos: 66.6,
+        azucar: 32.9,
+        sodio: 334,
+        imagen: 'assets/images/dulces/galletas/costa_chocochips.png'
+    },{
+        nombre: 'Selz Cracker', 
+        marca: 'Selz',
+        tipo: 'Cracker',
+        energia: 443,
+        proteina: 11,
+        grasa: 15,
+        carbohidratos: 66,
+        azucar: 0.8,
+        sodio: 689,
+        imagen: 'assets/images/dulces/galletas/selz_cracker.png'
+    },{
+        nombre: 'Selz Clásicas', 
+        marca: 'Selz',
+        tipo: 'Clásicas',
+        energia: 484,
+        proteina: 8.7,
+        grasa: 24,
+        carbohidratos: 59,
+        azucar: 6.6,
+        sodio: 883,
+        imagen: 'assets/images/dulces/galletas/selz_clasica.png'
+    },{
+        nombre: 'Vivo Salvado', 
+        marca: 'Vivo',
+        tipo: 'Salvado',
+        energia: 404,
+        proteina: 8.8,
+        grasa: 11.7,
+        carbohidratos: 65.9,
+        azucar: 9.9,
+        sodio: 393,
+        imagen: 'assets/images/dulces/galletas/vivo_salvado.png'
+    },
+
+];
+
+export const TIPOSPAPASFRITAS: Producto[] = [
+
+    {
+        nombre: 'Lays Corte americano', 
+        marca: 'Lays',
+        tipo: 'Corte americano',
+        energia: 552,
+        proteina: 4.4,
+        grasa: 34,
+        carbohidratos: 57,
+        azucar: 2,
+        sodio: 376,
+        imagen: 'assets/images/dulces/galletas/lays_corteamericano.png'
+    },{
+        nombre: 'Lays Stax Crema y Cebolla', 
+        marca: 'Lays',
+        tipo: 'Stax Crema y Cebolla',
+        energia: 490,
+        proteina: 5.9,
+        grasa: 26,
+        carbohidratos: 59,
+        azucar: 4,
+        sodio: 441,
+        imagen: 'assets/images/dulces/galletas/lays_cremacebolla.png'
+    },{
+        nombre: 'Kryspo Original', 
+        marca: 'Kryspo',
+        tipo: 'Original',
+        energia: 507,
+        proteina: 5.8,
+        grasa: 28,
+        carbohidratos: 58,
+        azucar: 0.5,
+        sodio: 380,
+        imagen: 'assets/images/dulces/galletas/kryzpo_original.png'
+    },{
+        nombre: 'Cuisine & Co Sal de mar', 
+        marca: 'Cuisine & Co',
+        tipo: 'Sal de mar',
+        energia: 488,
+        proteina: 7.4,
+        grasa: 25.9,
+        carbohidratos: 56.4,
+        azucar: 0.8,
+        sodio: 218,
+        imagen: 'assets/images/dulces/galletas/cuisine_saldemar.png'
+    },{
+        nombre: 'Tika Nativas andina', 
+        marca: 'Tika',
+        tipo: 'Nativas andina',
+        energia: 536,
+        proteina: 6.1,
+        grasa: 35,
+        carbohidratos: 49.1,
+        azucar: 0.1,
+        sodio: 247,
+        imagen: 'assets/images/dulces/galletas/tika_nativa.png'
+    },
+]
+
+export const TIPOSSNACK: Producto[] = [
+
+    {
+        nombre: 'Pancho Villa Panchitos', 
+        marca: 'Pancho Villa',
+        tipo: 'Panchitos',
+        energia: 488,
+        proteina: 9.4,
+        grasa: 23.4,
+        carbohidratos: 60.2,
+        azucar: 0,
+        sodio: 390,
+        imagen: 'assets/images/dulces/galletas/lays_corteamericano.png'
+    },{
+        nombre: 'Evercrips Original', 
+        marca: 'Evercrips',
+        tipo: 'Original',
+        energia: 533,
+        proteina: 8.4,
+        grasa: 26,
+        carbohidratos: 60,
+        azucar: 12,
+        sodio: 452,
+        imagen: 'assets/images/dulces/galletas/lays_corteamericano.png'
+    },
+]
